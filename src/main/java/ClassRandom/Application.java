11bacc1797af47e02eeabb23e939556267eebcbc
Quynhlethi <lequynh94.hrm.neu@gmail.com>
2018-07-17 10:16:16 +0700

    run program of class MyRandom

---
+++ src/main/java/ClassRandom/Application.java
@@ -0,0 +1,8 @@
+package ClassRandom;
+
+public class Application {
+    public static void main(String[] args) {
+       MyRandom random = new MyRandom();
+       random.printRandomNumbers();
+    }
+}
