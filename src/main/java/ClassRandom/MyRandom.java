666c29771110192c1f42542bb9c58a60d69ab979
Quynhlethi <lequynh94.hrm.neu@gmail.com>
2018-07-17 10:15:18 +0700

    add class MyRandom with seed 1000 and print 50 first numbers (0-100) use method nextInt()

---
+++ src/main/java/ClassRandom/MyRandom.java
@@ -0,0 +1,19 @@
+package ClassRandom;
+
+import java.util.Random;
+
+public class MyRandom {
+    Random random = new Random(1000);
+
+    public int randomNumber() {
+       return random.nextInt(100);
+    }
+
+    public void printRandomNumbers() {
+        System.out.printf("STT \t RandomNumber\n");
+        for (int i = 1; i <= 50; i++) {
+            System.out.printf("%d \t\t %d\n", i, randomNumber());
+        }
+    }
+
+}
